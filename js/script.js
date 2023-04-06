7f9bab352d77425e147eb4caf3e0c4c73df90fa3
yuvraj <yuvraj042003@gmail.com>
2023-04-07 01:07:27 +0530

    add feacture

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 // Open Menu Items
-menuIcon.onclick  = () => {
+menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
-} ;
+};
 
 // Scroll Section Active Link
 
@@ -18,8 +18,8 @@ window.onscroll = () => {
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
-        
-        if(top >= offset && top< offset + height){
+
+        if (top >= offset && top < offset + height) {
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
@@ -29,18 +29,18 @@ window.onscroll = () => {
     // Stickey Navbar
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
-    
+
     //  Remove icon and click on the new link 
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
-    ScrollReveal({
-        reset:true,
-        distance:'80px',
-        duration: 2000,
-        delay: 200
-    });
-    ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
-    ScrollReveal().reveal('.home-img, .service-container, .portfolio-box. contact form', {origin: 'bottom'});
-    ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
-    ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .service-container, .portfolio-box. contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
